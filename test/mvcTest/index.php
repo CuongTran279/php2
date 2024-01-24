5e113f8f2ac3f2f940d8f4d3ce47683b5de9e4b2
CuongTran279 <cuongtdph32393@fpt.edu.vn>
2024-01-25 01:01:39 +0700

    upload

---
+++ test/mvcTest/index.php
@@ -0,0 +1,4 @@
+<?php
+    require_once "app/common/router.php";
+    require_once "vendor/autoload.php";
+?>
---
+++ test/mvcTest/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cuong/mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5418ffe26fbc0276d5bb41eef8f05267deae1833',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cuong/mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5418ffe26fbc0276d5bb41eef8f05267deae1833',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phroute/phroute' => array(
+            'pretty_version' => 'v2.2.0',
+            'version' => '2.2.0.0',
+            'reference' => 'c1893b885966a0c7b50b9239dd867fda7a312dfa',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phroute/phroute',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
